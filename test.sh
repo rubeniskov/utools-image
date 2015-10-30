a92bff3f726fa84a2e94dcbd0e9f6152c1112d2a
Rubén López Gómez <loslopez1@icloud.com>
2015-10-30 17:46:25 +0100

    Add: unit test files

---
+++ test.sh
@@ -0,0 +1,3 @@
+source "test/unit_test_image_create.sh"
+source "test/unit_test_image_mount.sh"
+source "test/unit_test_image_umount.sh"
---
+++ test/unit_test_image_create.sh
@@ -0,0 +1,6 @@
+
+fn_image_create /tmp/test
+
+fn_image_create /tmp/test --volname test
+
+fn_image_create /tmp/test --volname test --size 20GB
